b075f8c8963f56f4326ecb10886ddc7ed3b5187e
simonyost <simonyost@gmail.com>
2016-04-25 13:40:01 -0500

    ready for lab

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// loop
+var count = 0;
+var list = '';
+
+while (count < 10) {
+  list += '<li>current count is: ' + count + '</li>';
+  count += 1;
+}
+
+// output results
+document.write("<h1>Loops</h1>");
+document.write("<ul>" + list + "</ul>");
